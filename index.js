c89c6ab1edef5da0b9887b5e884a7bc07200e6f8
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2015-10-24 14:06:46 -0700

    stuff

--- index.js
+++ index.js
@@ -5,17 +5,25 @@ var request = require('request');
 
 app.use(express.static('public'));
 
-var server = app.listen(3013, function () {
+app.set('port', (process.env.PORT || 5000));
+
+var RESPONSE_TIME = 50;
+
+var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  app.use(bodyParser.json())
+  app.use(bodyParser.json());
+
+  app.get('/response-time', function(req, res, next) {
+    setTimeout(function() {
+      res.send("Done!");
+    }, RESPONSE_TIME);
+  });
 
   app.post('/sendClick', function(req, res, next) {
-    // console.log(req.body.side);
-
     request({
-        url: "https://data-engine-52-89-233-160.jutdata.io:3110/api/v1/import/webhook/?space=button_game&data_source=button_game&apikey=k81VYAN8m-1v&field_connector=webhook&field_data_source=button_game",
+        url: process.env.JUT_WEBOOK_URL,
         method: "POST",
         json: true,
         contentType: "application/json",
