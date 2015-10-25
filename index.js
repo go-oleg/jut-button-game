d06aac7ed749286aa78f0580cfd2392603f2e7aa
Oleg Seletsky <oleg.b.seletsky@gmail.com>
2015-10-24 17:55:45 -0700

    cleanup

--- index.js
+++ index.js
@@ -7,19 +7,11 @@ app.use(express.static('public'));
 
 app.set('port', (process.env.PORT || 5000));
 
-var RESPONSE_TIME = 50;
-
 var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
 
   app.use(bodyParser.json());
-
-  app.get('/response-time', function(req, res, next) {
-    setTimeout(function() {
-      res.send("Done!");
-    }, RESPONSE_TIME);
-  });
 
   app.post('/sendClick', function(req, res, next) {
     request({
